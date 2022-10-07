e1d5001e5aa12306583da47c65dd4a144769e204
bwmr <b.wimmer@bioc.uzh.ch>
2022-10-07 12:18:29 +0200

    cryoCARE denoising now fully works with tomotools-generated folders, multiple GPUs, etc.

--- tomotools/commands/denoising.py
+++ tomotools/commands/denoising.py
@@ -5,10 +5,12 @@ import json
 import tarfile
 
 from os import path
+from os.path import isdir, join
 from pathlib import Path
 from glob import glob
 
 from tomotools.utils.tomogram import Tomogram
+from tomotools.utils.util import num_gpus
 
 @click.command()
 @click.option('--num_slices', type=int, default=1200, show_default=True, help='Number of sub-volume extracted per tomogram.')
@@ -33,7 +35,7 @@ def cryocare_extract(num_slices, split, patch_shape, tilt_axis, n_normalization_
     input_evn = list()
     input_odd = list()
     
-    if not path.isdir(output_path):
+    if not isdir(output_path):
         os.mkdir(output_path)
     
     # Convert all input_files into a list of Tomogram objects
@@ -42,10 +44,8 @@ def cryocare_extract(num_slices, split, patch_shape, tilt_axis, n_normalization_
         if input_file.is_file():
             input_tomo.append(Tomogram(Path(input_file)))
         elif input_file.is_dir():
-            input_tomo += ([Tomogram(Path(file))for file in glob(path.join(input_file, '*_rec_bin_[0-9].mrc'))])
+            input_tomo += ([Tomogram(Path(file))for file in glob(join(input_file, '*_rec_bin_[0-9].mrc'))])
             
-   
-    
     for tomo in input_tomo:
         if path.isfile(tomo.path.with_name(f'{tomo.path.stem}_EVN.mrc')) and path.isfile(tomo.path.with_name(f'{tomo.path.stem}_ODD.mrc')):
             tomo = tomo.with_split_files(tomo.path.with_name(f'{tomo.path.stem}_EVN.mrc'), tomo.path.with_name(f'{tomo.path.stem}_ODD.mrc'))
@@ -65,12 +65,12 @@ def cryocare_extract(num_slices, split, patch_shape, tilt_axis, n_normalization_
 @click.command()
 @click.option('--epochs', type=int, default=100, show_default=True)
 @click.option('--steps_per_epoch', type=int, default=200, show_default=True)
-@click.option('--batch_size', type=int, default=16, show_default=True)
+@click.option('--batch_size', type=int, default=16, show_default=True, help='Increase if multiple GPUs are used. Try 32 or 64, depending on available memory.')
 @click.option('--unet_kern_size', type=int, default=3, show_default=True, help='Convolution kernel size of the U-Net. Has to be odd.')
 @click.option('--unet_n_depth', type=int, default=3, show_default=True)
 @click.option('--unet_n_first', type=int, default=16, show_default=True, help='Number of initial feature channels.')
 @click.option('--learning_rate', type=float, default=0.0004, show_default=True)
-@click.option('--gpu', type=str, default="0", show_default=True, help='Specify which GPUs to use. Not functional yet, sorry!')
+@click.option('--gpu', type=str, default=None, help='Specify which GPUs to use. Default: All GPUs.')
 @click.argument('extraction_dir', type=click.Path(dir_okay=True, file_okay=False))
 @click.argument('training_dir', type=click.Path(dir_okay=True, file_okay=False), default='./')
 @click.argument('model_name', type=str, default='model')
@@ -83,10 +83,17 @@ def cryocare_train(epochs, steps_per_epoch, batch_size, unet_kern_size, unet_n_d
     from cryocare.internals.CryoCARE import CryoCARE
     from csbdeep.models import Config
     from cryocare.internals.CryoCAREDataModule import CryoCARE_DataModule
-    #from cryocare.scripts.cryoCARE_predict import set_gpu_id
+    from cryocare.scripts.cryoCARE_predict import set_gpu_id
 
-    # Multi-GPU is in repository, but not yet available as of version 0.2
-    #set_gpu_id({'gpu_id': gpu.split(',')})
+    if gpu is None:
+        gpu_id = [int(i) for i in range(0,num_gpus())]
+
+    else:
+        # Turn GPU list into list of integers
+        gpu_id = gpu.split(',')
+        gpu_id = [int(gpu) for gpu in gpu_id]
+
+    set_gpu_id({'gpu_id': gpu_id})
 
     dm = CryoCARE_DataModule()
     dm.load(extraction_dir)
@@ -101,8 +108,7 @@ def cryocare_train(epochs, steps_per_epoch, batch_size, unet_kern_size, unet_n_d
         unet_n_depth=unet_n_depth,
         unet_n_first=unet_n_first,
         train_tensorboard=False,
-        train_learning_rate=learning_rate,
-        gpu_id=gpu.split(',')
+        train_learning_rate=learning_rate
     )
 
     model = CryoCARE(net_conf, model_name, basedir=training_dir)
@@ -110,7 +116,7 @@ def cryocare_train(epochs, steps_per_epoch, batch_size, unet_kern_size, unet_n_d
     history = model.train(dm.get_train_dataset(), dm.get_val_dataset())
     mean, std = dm.train_dataset.mean, dm.train_dataset.std
 
-    with open(path.join(training_dir, model_name, 'history.dat'), 'wb+') as f:
+    with open(join(training_dir, model_name, 'history.dat'), 'wb+') as f:
         pickle.dump(history.history, f)
 
     norm = {
@@ -118,67 +124,97 @@ def cryocare_train(epochs, steps_per_epoch, batch_size, unet_kern_size, unet_n_d
         "std": float(std)
     }
     
-    with open(path.join(training_dir, model_name, 'norm.json'), 'w') as fp:
+    with open(join(training_dir, model_name, 'norm.json'), 'w') as fp:
         json.dump(norm, fp)
 
-    with tarfile.open(path.join(training_dir, f"{model_name}.tar.gz"), "w:gz") as tar:
-        tar.add(path.join(training_dir, model_name), arcname=path.basename(path.join(training_dir, model_name)))
+    with tarfile.open(join(training_dir, f"{model_name}.tar.gz"), "w:gz") as tar:
+        tar.add(join(training_dir, model_name), arcname=path.basename(join(training_dir, model_name)))
 
 @click.command()
-@click.option('--tiles', type=(int, int, int), default=[1, 2, 2], show_default=True, help='Specify number of tiles.')
-@click.option('--model-path', type=click.Path(exists=True), default='./', show_default=True, help='Specify the folder containing the model.')
-@click.option('--output', type=click.Path(dir_okay=True), default='denoised', help='Specify the output directory for denoised tomograms.')
-@click.argument('tomogram', type=click.Path(dir_okay=False, exists=True))
+@click.option('--tiles', type=(int,int,int), default=(1, 1, 1), show_default=True, help='Specify number of tiles.')
+@click.option('--gpu', type=str, default=None, help='Specify which GPUs to use. Default: All GPUs.')
+@click.option('--model-path', type=click.Path(dir_okay = False, exists=True), default='./model.tar.gz', show_default=True, help='Specify the folder containing the model.')
+@click.argument('input_files', nargs=-1, type=click.Path(exists=True), )
+@click.argument('output', type=click.Path(dir_okay=True), default='./denoised')
 
-def cryocare_predict(tiles, model_path, output, tomogram):
+def cryocare_predict(tiles, gpu, model_path, input_files, output):
     """ Predicts denoised tomogram using cryoCARE.
     
-    Takes tomogram with associated EVN/ODD halves and the trained model as inputs.
+    Takes tomograms or folder containing them with associated EVN/ODD halves and the trained model as inputs.
     """
-    from cryocare.internals.CryoCAREDataModule import CryoCARE_DataModule
-    from cryocare.internals.CryoCARE import CryoCARE
-    import mrcfile
-    import numpy as np
-    import datetime
+
+    import tempfile
     
-    tomogram = Path(tomogram)
+    from cryocare.scripts.cryoCARE_predict import set_gpu_id, denoise
+
+    # Parse input tomogram        
+    input_tomo = list()
+    input_evn = list()
+    input_odd = list()
     
-    if path.isfile(tomogram.with_name(f'{tomogram.stem}_EVN.mrc')) and path.isfile(tomogram.with_name(f'{tomogram.stem}_ODD.mrc')):
-        tomo = Tomogram(tomogram).with_split_files(tomogram.with_name(f'{tomogram.stem}_EVN.mrc'), tomogram.with_name(f'{tomogram.stem}_ODD.mrc'))
-        print(f'Found reconstruction {tomo.path} with EVN and ODD stacks.')
-    else:
-        print('Tomogram EVN/ODD halves not found.')
+    # Convert all input_files into a list of Tomogram objects
+    for input_file in input_files:
+        input_file = Path(input_file)
+        if input_file.is_file():
+            input_tomo.append(Tomogram(Path(input_file)))
+        elif input_file.is_dir():
+            input_tomo += ([Tomogram(Path(file))for file in glob(join(input_file, '*_rec_bin_[0-9].mrc'))])
+            
+    for tomo in input_tomo:
+        if path.isfile(tomo.path.with_name(f'{tomo.path.stem}_EVN.mrc')) and path.isfile(tomo.path.with_name(f'{tomo.path.stem}_ODD.mrc')):
+            tomo = tomo.with_split_files(tomo.path.with_name(f'{tomo.path.stem}_EVN.mrc'), tomo.path.with_name(f'{tomo.path.stem}_ODD.mrc'))
+            input_evn.append(tomo.evn_path)
+            input_odd.append(tomo.odd_path)
+            print(f'Found reconstruction {tomo.path} with EVN and ODD stacks.')
+        else:
+            print(f'No EVN/ODD reconstructions found for {tomo.path}. Skipping.')
     
+    # Create output directory
     if not path.isdir(output):
         os.mkdir(output)
+        
+    # Take care of GPU stuff
+    if gpu is None:
+        gpu_id = [int(i) for i in range(0,num_gpus())]
+
+    else:
+        # Turn GPU list into list of integers
+        gpu_id = gpu.split(',')
+        gpu_id = [int(gpu) for gpu in gpu_id]
+
+    set_gpu_id({'gpu_id': gpu_id})
     
-    dm = CryoCARE_DataModule()
-    dm.load(model_path)
+    # Extract model and denoise   
+    if os.path.isfile(model_path):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            tar = tarfile.open(model_path, "r:gz")
+            tar.extractall(tmpdirname)
+            tar.close()
+            
+            config = {}
+            config['even'] = input_evn
+            config['odd'] = input_odd
+            config['output'] = output
+            config['n_tiles'] = list(tiles)
+            
+            config['model_name'] = os.listdir(tmpdirname)[0]
+            config['path'] = join(tmpdirname)
+            
+            with open(join(tmpdirname,config['model_name'],"norm.json")) as f:
+                norm_data = json.load(f)
+                mean = norm_data["mean"]
+                std = norm_data["std"]
 
-    model = CryoCARE(None, model_path, basedir=model_path)
+            if type(config['even']) is list:
+                all_even=tuple(config['even'])
+                all_odd=tuple(config['odd'])
+            elif os.path.isdir(config['even']) and os.path.isdir(config['odd']):
+                all_even = glob(join(config['even'],"*.mrc"))
+                all_odd = glob(join(config['odd'],"*.mrc"))
+            else:
+                all_even = [config['even']]
+                all_odd = [config['odd']]
 
-    even = mrcfile.mmap(tomo.evn_path, mode='r', permissive=True)
-    odd = mrcfile.mmap(tomo.odd_path, mode='r', permissive=True)
-    denoised = mrcfile.new_mmap(path.join(model_path, output), even.data.shape, mrc_mode=2,
-                                overwrite=True)
-
-    even.data.shape += (1,)
-    odd.data.shape += (1,)
-    denoised.data.shape += (1,)
-
-    mean, std = dm.train_dataset.mean, dm.train_dataset.std
-
-    model.predict(even.data, odd.data, denoised.data, axes='ZYXC', normalizer=None, mean=mean, std=std,
-                  n_tiles=list(tiles) + [1, ])
-
-    for label in even.header.dtype.names:
-        if label == 'label':
-            new_label = np.concatenate((even.header[label][1:-1], np.array([
-                'cryoCARE                                                ' + datetime.datetime.now().strftime(
-                    "%d-%b-%y  %H:%M:%S") + "     "]),
-                                        np.array([''])))
-            print(new_label)
-            denoised.header[label] = new_label
-        else:
-            denoised.header[label] = even.header[label]
-    denoised.header['mode'] = 2
+            for even,odd in zip(all_even,all_odd):
+                out_filename = join(config['output'], os.path.basename(even))
+                denoise(config, mean, std, even=even, odd=odd, output_file=out_filename)
