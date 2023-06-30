c9568878a7664f59e82874fb9c5a2d69ca05706a
bwmr <56264458+bwmr@users.noreply.github.com>
2023-06-30 16:06:03 +0200

    added functions to fix up / create tilt.com and ctfcorrection.com for ctf3dsetup

--- tomotools/utils/comfile.py
+++ tomotools/utils/comfile.py
@@ -1,7 +1,12 @@
+from tomotools.utils.tiltseries import TiltSeries
+
+
 def get_value(path, key):
     with open(path) as file:
         for line in file:
             if line.startswith(f'{key}\t'):
+                return line.split()[1].strip()
+            elif line.startswith(f'{key} '):
                 return line.split()[1].strip()
     return None
 
@@ -17,3 +22,71 @@ def modify_value(path, key, value):
             lines[n] = f'{key} {value}\n'
     with open(path, 'w') as file:
         file.writelines(lines)
+
+
+def remove_value(path, key):
+    lines = list()
+    lines_cleaned = list()
+
+    with open(path) as file:
+        lines = file.readlines()
+    for n, line in enumerate(lines):
+        if line.startswith(f'{key}\t'):
+            continue
+        elif line.startswith(f'{key} '):
+            continue
+        else:
+            lines_cleaned.append(line)
+        
+    with open(path, 'w') as file:
+        file.writelines(lines_cleaned)
+
+
+def fake_ctfcom(ts: TiltSeries, binning: int):
+    '''
+    Create ctfcorrection.com file for your reconstruction folder.
+
+    Does not check whether the required files are there!
+    Right now, 300 kV and 2.7 mm Cs are hard-coded.
+    '''
+
+    content = ['# Command file to run ctfphaseflip',
+               '# Created with tomotools',
+               '$setenv IMOD_OUTPUT_FORMAT MRC',
+               '$ctfphaseflip -StandardInput',
+               f'InputStack  {ts.path.stem}_ali.mrc',
+               f'AngleFile   {ts.path.with_suffix(".tlt").name}',
+               f'OutputFileName	{ts.path.with_name(ts.path.stem).name}_ctfcorr.mrc',
+               f'TransformFile   {ts.path.with_suffix(".tlt").name}',
+               f'DefocusFile    {ts.path.with_suffix(".defocus").name}',
+               'Voltage 300',
+               'SphericalAberration 2.7',
+               'DefocusTol	50',
+               f'PixelSize {ts.angpix()*binning/10}',
+               'AmplitudeContrast	0.07',
+               'InterpolationWidth	15',
+               'ActionIfGPUFails	1,2',
+               '$if (-e ./savework) ./savework']
+
+    with open(ts.path.with_name("ctfcorrection.com"), 'w') as file:
+        file.truncate(0)
+        file.write('\n'.join(content))
+
+
+def fix_tiltcom(ts: TiltSeries, thickness: int, fsirt: int, bin: int):
+    '''
+    Make sure tilt.com file has the right parameters.
+
+    AreTomo irritatingly puts the alignment file as LOCALFILE, remove this.
+
+    '''
+
+    modify_value(ts.path.with_name('tilt.com'), 'IMAGEBINNED', str(bin))
+    modify_value(ts.path.with_name('tilt.com'), 'THICKNESS', str(thickness))
+    modify_value(ts.path.with_name('tilt.com'), 'InputProjections', f'{ts.path.stem}_ali.mrc')
+    
+    if get_value(ts.path.with_name("tilt.com"), 'FakeSIRTiterations') is not None:
+        modify_value(ts.path.with_name('tilt.com'), 'FakeSIRTiterations', str(fsirt))
+    
+    if get_value(ts.path.with_name("tilt.com"), 'LOCALFILE') == ts.path.with_suffix(".xf").name:
+        remove_value(ts.path.with_name('tilt.com'), 'LOCALFILE')
