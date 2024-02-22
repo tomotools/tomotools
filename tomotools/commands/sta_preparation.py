5a1fea74dd7663d64f910366b505c9ea0dd663a9
Alice Thurston <63013027+alicethurston@users.noreply.github.com>
2024-02-22 10:38:34 +0100

    Update sta_preparation, enable bin rounding options for x2tomotwin commands

--- tomotools/commands/sta_preparation.py
+++ tomotools/commands/sta_preparation.py
@@ -142,13 +142,13 @@ def aretomo2warp(batch_input, name, input_files, project_dir):
               help="Read input files as text, each line is a tiltseries (folder)")
 @click.option('-d', '--thickness', default=3000, show_default=True,
               help="Tomogram thickness in unbinned pixels.")
-@click.option('-r', '--bin-round-down', is_flag=True, default=False, show_default=True,
-              help="Using flag forces math.floor instead of math.ceil in the binning calculation.")
+@click.option('--bin-round-up/--bin-round-down', is_flag=True, default=True, show_default=True,
+              help="Default calculats binning closest to 10A and rounds up. --bin-round-down forces rounding down.")
 @click.option('--uid', default='imod', show_default=True,
               help="Unique identifier to tell apart tomograms from several sessions.")
 @click.argument('input_files', nargs=-1)
 @click.argument('tomotwin_dir', nargs=1)
-def imod2tomotwin(batch_input, thickness, bin_round_down, uid, input_files, tomotwin_dir):
+def imod2tomotwin(batch_input, thickness, bin_round_up, uid, input_files, tomotwin_dir):
     """Prepare for TomoTwin picking.
 
     Takes as input several tiltseries (folders) or a file listing them (with -b),
@@ -163,7 +163,7 @@ def imod2tomotwin(batch_input, thickness, bin_round_down, uid, input_files, tomo
     """
     ts_list = sta_util.batch_parser(input_files, batch_input)
 
-    sta_util.tomotwin_prep(tomotwin_dir, ts_list, thickness, uid, bin_round_down=bin_round_down)
+    sta_util.tomotwin_prep(tomotwin_dir, ts_list, thickness, uid, bin_round_up=bin_round_up)
 
 
 @click.command()
@@ -171,11 +171,13 @@ def imod2tomotwin(batch_input, thickness, bin_round_down, uid, input_files, tomo
               help="Read input files as text, each line is a tiltseries (folder)")
 @click.option('-d', '--thickness', default=3000, show_default=True,
               help="Tomogram thickness in unbinned pixels.")
+@click.option('--bin-round-up/--bin-round-down', is_flag=True, default=True, show_default=True,
+              help="Default calculats binning closest to 10A and rounds up. --bin-round-down forces rounding down.")
 @click.option('--uid', default='aretomo', show_default=True,
               help="Unique identifier to tell apart tomograms from several sessions.")
 @click.argument('input_files', nargs=-1)
 @click.argument('tomotwin_dir', nargs=1)
-def aretomo2tomotwin(batch_input, thickness, uid, input_files, tomotwin_dir):
+def aretomo2tomotwin(batch_input, thickness, bin_round_up, uid, input_files, tomotwin_dir):
     """Prepare for TomoTwin picking.
 
     Takes as input several tiltseries (folders) or a file listing them (with -b),
@@ -201,4 +203,4 @@ def aretomo2tomotwin(batch_input, thickness, uid, input_files, tomotwin_dir):
         ts_imodlike.append(sta_util.aretomo_export(ts))
 
     # Process as normal imod-aligned TS
-    sta_util.tomotwin_prep(tomotwin_dir, ts_imodlike, thickness, uid)
+    sta_util.tomotwin_prep(tomotwin_dir, ts_imodlike, thickness, uid, bin_round_up=bin_round_up
